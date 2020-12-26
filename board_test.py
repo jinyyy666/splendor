55bd265b09ba5886a7f5251f3df4f0f9340d8114
jes2ica <jes2icazhuang@gmail.com>
2020-12-26 10:24:50 -0800

    update unit tests

--- board_test.py
+++ board_test.py
@@ -3,34 +3,53 @@
 from board import Board
 from model import Gem
 
+import unittest
 
-def test_load():
-    b = Board(2)
-    assert len(b.all_cards[0]) == 40
-    assert len(b.all_cards[1]) == 30
-    assert len(b.all_cards[2]) == 20
 
-    assert len(b.all_nobles) == 10
+class BoardTest(unittest.TestCase):
+    def test_load(self):
+        b = Board(2)
+        self.assertEqual(len(b.all_cards[0]), 40)
+        self.assertEqual(len(b.all_cards[1]), 30)
+        self.assertEqual(len(b.all_cards[2]), 20)
 
-    assert len(b.players) == 2
+        self.assertEqual(len(b.all_nobles), 10)
 
-    assert len(b.gems) == 6
-    assert b.gems[Gem.RED] == 4
-    assert b.gems[Gem.GOLD] == 5
+        self.assertEqual(len(b.players), 2)
 
-    assert len(b.nobles) == 3
+        self.assertEqual(len(b.gems), 6)
 
-    assert len(b.cards) == 3
-    assert len(b.cards[0]) == 4
+        self.assertEqual(b.gems[Gem.RED], 4)
+        self.assertEqual(b.gems[Gem.GOLD], 5)
 
-def test_take_card():
-    b = Board(2)
-    card = b.cards[0][0]
-    b.take_card(card.id)
-    assert len(b.cards[0]) == 4
+        self.assertEqual(len(b.nobles), 3)
+        self.assertEqual(len(b.cards), 3)
+        self.assertEqual(len(b.cards[0]), 4)
+
+    def test_take_card(self):
+        b = Board(2)
+        card = b.cards[0][0]
+        b.take_card(card.id)
+        self.assertEqual(len(b.cards[0]), 4)
+
+    def test_take_gems(self):
+        b = Board(2)
+        gems = {}
+        gems[Gem.RED] = 1
+        gems[Gem.GREEN] = 1
+        gems[Gem.BLUE] = 1
+        b.take_gems(gems)
+        self.assertEqual(len(b.gems), 6)
+        self.assertEqual(b.gems[Gem.RED], 3)
+
+    def test_not_enough_gems(self):
+        b = Board(2)
+        gems = {}
+        gems[Gem.RED] = 5
+        with self.assertRaises(ValueError):
+            b.take_gems(gems)
 
 
 if __name__ == "__main__":
-    test_load()
-    test_take_card()
+    unittest.main()
     print("Everything passed")
--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-def can_get_gem(self, all_gems, gems):
+def can_get_gem(all_gems, gems):
     for gem_t, cnt in gems.items():
         if cnt > all_gems[gem_t]:
             return False
