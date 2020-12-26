cd026735f8a1a92c151115c4c05cbffb6a287dbb
Jimmy Jin <7366393+jinyyy666@users.noreply.github.com>
2020-12-26 10:47:40 -0800

    resturcture test files

--- test/player_test.py
+++ test/player_test.py
@@ -1,22 +1,33 @@
 #! /usr/local/bin/python3
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+import unittest
 
 from board import Board
 from player import Player
+from model import (
+    Gem,
+) 
+
+class PlayerTest(unittest.TestCase):
 
 
-def test_player_sanity():
-    player1 = Player(0)
-    player2 = Player(1)
+    def test_player_sanity(self):
+        player1 = Player(0)
+        player2 = Player(1)
 
-    assert (player1.get_id() == 0)
-    assert (player2.get_id() == 1)
+        assert (player1.get_id() == 0)
+        assert (player2.get_id() == 1)
 
+        assert (player1.card_summary() == {})
 
-#def test_
+    def test_pick_same_gems(self):
+        my_board = Board(2)
+        gems = {
+            Gem.RED : 2
+        }
+
 
 
 if __name__ == "__main__":
-    test_player_sanity()
+    unittest.main()
     print("Everything passed for players!")
